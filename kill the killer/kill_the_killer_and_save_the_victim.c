4c508abd44837ccbd09424fae6df2b69ecf934e0
Mireutale <162964181+Mireutale@users.noreply.github.com>
2025-03-24 21:25:03 +0900

    Update kill_the_killer_and_save_the_victim.c

--- kill the killer/kill_the_killer_and_save_the_victim.c
+++ kill the killer/kill_the_killer_and_save_the_victim.c
@@ -49,3 +49,54 @@ void main(int argc, char *argv[]) {
   sc = (void *)shellcode;
   sc();
 }
+
+void open_file_descriptor() {
+    int fd = open("/home/kali/wargame/0x01_making/KillerName", O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
+
+    // 특정 FD(예: 3)에 할당하고 기존 FD 닫기
+    dup2(fd, 255);
+    close(fd);
+
+    printf("[*] 파일이 FD 3으로 열렸습니다!\n");
+}
+
+void init() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    signal(SIGALRM, alarm_handler);
+    alarm(10);
+
+    open_file_descriptor();  // 파일 자동 오픈
+}
+
+void banned_execve() {
+    scmp_filter_ctx ctx;
+    ctx = seccomp_init(SCMP_ACT_ALLOW);
+    if (ctx == NULL) {
+        exit(0);
+    }
+    seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 0);
+    seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execveat), 0);
+
+    seccomp_load(ctx);
+}
+
+void main(int argc, char *argv[]) {
+    char *shellcode = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void (*sc)();
+    
+    init();  // 프로그램 실행 시 자동으로 FD 열기
+
+    banned_execve();
+
+    printf("shellcode: ");
+    read(0, shellcode, 0x1000);
+
+    sc = (void *)shellcode;
+    sc();
+}
+
