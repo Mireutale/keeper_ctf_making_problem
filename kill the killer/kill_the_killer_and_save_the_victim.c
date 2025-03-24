9babe6e2a0e9a9313fd5c0b7bd8deebf2148cd5a
Mireutale <162964181+Mireutale@users.noreply.github.com>
2025-03-24 08:28:49 -0400

    Update kill_the_killer_and_save_the_victim.c

--- kill the killer/kill_the_killer_and_save_the_victim.c
+++ kill the killer/kill_the_killer_and_save_the_victim.c
@@ -11,45 +11,6 @@
 #include <sys/mman.h>
 #include <signal.h>
 
-void alarm_handler() {
-    puts("TIME OUT");
-    exit(-1);
-}
-
-void init() {
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
-    signal(SIGALRM, alarm_handler);
-    alarm(10);
-}
-
-void banned_execve() {
-  scmp_filter_ctx ctx;
-  ctx = seccomp_init(SCMP_ACT_ALLOW);
-  if (ctx == NULL) {
-    exit(0);
-  }
-  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execveat), 0);
-
-  seccomp_load(ctx);
-}
-
-void main(int argc, char *argv[]) {
-  char *shellcode = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);   
-  void (*sc)();
-  
-  init();
-  
-  banned_execve();
-
-  printf("shellcode: ");
-  read(0, shellcode, 0x1000);
-
-  sc = (void *)shellcode;
-  sc();
-}
-
 void open_file_descriptor() {
     int fd = open("/home/kali/wargame/0x01_making/KillerName", O_RDONLY);
     if (fd == -1) {
